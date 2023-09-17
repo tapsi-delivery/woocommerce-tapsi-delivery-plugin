d4dc189c0a47e47400286542b420eade0b44441a
maryam.alikarami <maryam.alikarami@tap30.net>
2023-09-17 13:42:11 +0330

    fix: save pin location to form on map move

--- tapsi-delivery/admin/partials/wctd-taps-pack-maplibre-map.php
+++ tapsi-delivery/admin/partials/wctd-taps-pack-maplibre-map.php
@@ -10,13 +10,13 @@
 	 * @subpackage maplibre-js
 	 */
 
-	echo '<p>' . __( 'Please choose the precise coordinate that matches your address on the map.', 'tapsi-delivery' ) . '</p>';
-	echo '<p class="wctd-tapsi-pack-delivery-map-notification privacy_requests">' . __( "Make sure to check the pin location with your address. The pin location is Tapsi Pack's reference for delivering your package.", 'tapsi-delivery' ) . '</p>';
 	echo '<section class="wctd-tapsi-pack-map-form">';
     echo '<p class="form-row"><label for="wctd-tapsi-pack-maplibre-map-container-id">'. __('Coordinates', 'tapsi-delivery') .'</label>&nbsp;<abbr class="required" title="' . esc_attr__( 'required', 'woocommerce' ) . '">*</abbr></p>';
+	echo '<p>' . __( 'Please choose the precise coordinate that matches your address on the map.', 'tapsi-delivery' ) . '</p>';
+	echo '<p class="wctd-tapsi-pack-delivery-map-notification privacy_requests">' . __( "Make sure to check the compatibility between the address and the pin. In case of incompatibility, The pin location is Tapsi Pack's reference for your delivery!!!", 'tapsi-delivery' ) . '</p>';
 	echo '<div id="wctd-tapsi-pack-maplibre-map-root-id">';
 	echo '<div id="wctd-tapsi-pack-maplibre-map-container-id"></div>';
-	echo '<button id="wctd-tapsi-pack-maplibre-map-submit-location-button-id">submit location</button>';
+//	echo '<button id="wctd-tapsi-pack-maplibre-map-submit-location-button-id">submit location</button>';
 	echo '<img id="wctd-tapsi-pack-maplibre-map-center-marker-id" src="http://localhost:9700/map-center-marker.svg"/>';
 	echo '</div>';
 	echo '<script src="http://localhost:9700/map-handler.js"></script>';

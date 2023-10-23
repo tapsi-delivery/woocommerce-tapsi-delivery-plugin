046a806351287f4a64e97a066d0d9c04531fd572
chamrun.moeini <chrm@aut.ac.ir>
2023-10-23 20:42:53 +0330

    chore(release): bump version to `0.1.4`

--- woo-tapsi-delivery/includes/class-woo-tapsi-delivery.php
+++ woo-tapsi-delivery/includes/class-woo-tapsi-delivery.php
@@ -90,7 +90,7 @@ class Woocommerce_Tapsi
         if (defined('WOOCOMMERCE_TAPSI_VERSION')) {
             $this->version = WOOCOMMERCE_TAPSI_VERSION;
         } else {
-            $this->version = '0.1.0';
+            $this->version = '0.1.4';
         }
         $this->plugin_name = 'woo-tapsi-delivery';
 

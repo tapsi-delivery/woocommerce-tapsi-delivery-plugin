7f8ae36aae68075f735a1f14183f083059a345f9
chamrun.moeini <chrm@aut.ac.ir>
2023-12-13 10:18:27 +0330

    chore(release): bump version to `0.1.10`

--- woo-tapsi-delivery/includes/class-woo-tapsi-delivery.php
+++ woo-tapsi-delivery/includes/class-woo-tapsi-delivery.php
@@ -90,7 +90,7 @@ class Woocommerce_Tapsi
         if (defined('WOOCOMMERCE_TAPSI_VERSION')) {
             $this->version = WOOCOMMERCE_TAPSI_VERSION;
         } else {
-            $this->version = '0.1.9';
+            $this->version = '0.1.10';
         }
         $this->plugin_name = 'woo-tapsi-delivery';
 
--- woo-tapsi-delivery/readme.txt
+++ woo-tapsi-delivery/readme.txt
@@ -5,7 +5,7 @@ Tags: woocommerce, shipping, delivery, tapsi
 Requires at least: 3.0.1
 Requires PHP: 7.4
 Tested up to: 6.3
-Stable tag: 0.1.9
+Stable tag: 0.1.10
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
